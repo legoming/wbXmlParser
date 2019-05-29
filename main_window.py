3129ecd73bd704d3589e39bac64e9836dc6f32a1
Jet CHEN <legoming@gmail.com>
2019-05-29 20:51:24 +0800

    rework to working version

--- main_window.py
+++ main_window.py
@@ -1,57 +1,57 @@
 # -*- coding: utf-8 -*-
 
-import sys
+import binascii
 import os
 import platform
 import subprocess
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QLabel, QMenuBar, QTextEdit, QAction, QFileDialog, QTableWidgetItem, QPushButton, QDialog, QMessageBox, QDesktopWidget, QVBoxLayout, QHBoxLayout, QTableWidget, QWidget
-from PyQt5.QtCore import QPoint, QTimer
-from PyQt5.QtGui import QIcon, QFont, QBrush, QColor, QPalette
-from enum import Enum, unique
-from lxml import etree
+import sys
+
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtCore import QTimer, Qt
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QLabel, QMenuBar, QTextEdit, QVBoxLayout, QHBoxLayout, QWidget
+
 # import wbxml # wbxml only support python2, and cannot install properly on mojave
 
 sys.setrecursionlimit(10000)
 
 dirWorking = os.getcwd()
-#print("current working dir is " + dirWorking)
+# print("current working dir is " + dirWorking)
 try:
-	dirScript = sys._MEIPASS
-	#print("current running dir of tool is " + dirScript)
+    dirScript = sys._MEIPASS
+# print("current running dir of tool is " + dirScript)
 except Exception:
-	dirScript = os.path.dirname(os.path.realpath(__file__))
-	#print("current running dir of script is " + dirScript)
+    dirScript = os.path.dirname(os.path.realpath(__file__))
+# print("current running dir of script is " + dirScript)
+
 
 class Ui_MainWindow(object):
-    def textInput1Changed(self):
-        self.timer1.start(1500)
+    def text_inputChanged(self):
+        self.timer_wait_to_refresh.start(1500)
 
-    def time1Timeout(self):
-        self.timer1.stop()
-        a_input = self.textInput1.toPlainText()
-        print(a_input)
-        cmd = '/usr/local/Cellar/libwbxml/0.11.6_1/bin/wbxml2xml -l WML10 -m0'
+    def refresh_output_when_timeout(self):
+        self.timer_wait_to_refresh.stop()
+        a_input = self.text_input.toPlainText()
+        print('\ninputed data\n' + a_input)
+        cmd = 'wbxml2xml'
         try:
             ishex = int(a_input[:5], 16)
-            label = 'wbxml -> xml'
-            print(label)
-            a_input = a_input.strip()
-            print(a_input)
+            finput = open(dirScript + '/input_file', 'wb')
+            a_input = "".join(a_input.split())
+            a = [a_input[i:i + 2] for i in range(0, len(a_input), 2)]
+            for aa in a:
+                finput.write(binascii.unhexlify(aa))
+            finput.close()
         except Exception as e:
-            label = 'xml -> wbxml'
-            print(label)
-            cmd = '/usr/local/Cellar/libwbxml/0.11.6_1/bin/xml2wbxml -a -v 1.0'
-        cmd = cmd + ' -o ' + dirScript + '/output_file ' + dirScript + '/input_file'
-        finput = open(dirScript + '/input_file', 'w')
-        finput.writelines(a_input)
-        finput.close()
-        output = subprocess.getstatusoutput(cmd)
-        print(cmd)
-        print(output)
-        self.lable2.setText("Parsed " + label + " Done") if output[0] == 0 else self.lable2.setText("Parsed " + label + " Failed")
+            finput = open(dirScript + '/input_file', 'w')
+            finput.writelines(a_input)
+            finput.close()
+            cmd = 'xml2wbxml'
+        fullcmd = cmd + ' -o ' + dirScript + '/output_file ' + dirScript + '/input_file'
+        output = subprocess.getstatusoutput(fullcmd)
+        self.label_parse_result.setText(cmd) if output[0] == 0 else self.label_parse_result.setText(cmd + " failed")
         if output[0] == 0:
-            if label == "xml -> wbxml":
+            if cmd == "xml2wbxml":
                 try:
                     with open(dirScript + '/output_file', "rb") as binary_file:
                         # Read the whole file at once
@@ -59,62 +59,78 @@ class Ui_MainWindow(object):
                         chs = ""
                         for ch in data:
                             chs += "{0:02x}".format(ch)
-                        self.textInput2.setText(chs)
+                        print('\nconverted wbxml\n' + chs)
+                        self.text_output.setText(chs)
                 except Exception as e:
                     print(str(e))
-                    self.textInput2.setText('Exception:\n' + str(e))
+                    self.text_output.setText('Exception:\n' + str(e))
+            else:
+                try:
+                    with open(dirScript + '/output_file', 'r') as xml_file:
+                        data = xml_file.read()
+                        print('\nconverted xml\n' + data)
+                        self.text_output.setPlainText(data)
+                except Exception as e:
+                    print(str(e))
+                    self.text_output.setText('Exception:\n' + str(e))
         else:
-            self.textInput2.setText(output[1])
+            self.text_output.setText(output[1])
 
-
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1024, 600)
-        # self.centralwidget = QtWidgets.QWidget(MainWindow)
+    def setupUi(self, main_window):
+        main_window.setObjectName("main_window")
+        main_window.resize(1024, 600)
+        # self.centralwidget = QtWidgets.QWidget(main_window)
         # self.centralwidget.setObjectName("centralwidget")
-        # MainWindow.setCentralWidget(self.centralwidget)
-        font = QFont()
-        font.setPointSize(15)
+        # main_window.setCentralWidget(self.centralwidget)
+        font = QFont('Roboto')
+        font.setPointSize(16)
 
-        self.menubar = QMenuBar(MainWindow)
+        fontmono = QFont('SF Mono')
+
+        self.menubar = QMenuBar(main_window)
         if platform.uname().system == 'Darwin':
             self.menubar.setNativeMenuBar(True)
-        #self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 50))
         self.menubar.setFont(font)
         self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        main_window.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(main_window)
         self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        main_window.setStatusBar(self.statusbar)
 
         self.subtitle = QHBoxLayout()
-        self.lable1 = QLabel()
-        self.lable1.setText("Copy paste WBXML or XML here")
+        self.label_input = QLabel()
+        self.label_input.setFont(font)
+        self.label_input.setAlignment(Qt.AlignCenter)
+        self.label_input.setText("copy paste XML/WBXML here")
 
-        self.lable2 = QLabel()
-        self.lable2.setText("Parsed Output")
+        self.label_parse_result = QLabel()
+        self.label_parse_result.setFont(font)
+        self.label_parse_result.setAlignment(Qt.AlignCenter)
+        self.label_parse_result.setText("converted output")
 
-        self.subtitle.addWidget(self.lable1)
-        self.subtitle.addWidget(self.lable2)
+        self.subtitle.addWidget(self.label_input)
+        self.subtitle.addWidget(self.label_parse_result)
 
-        self.textInput1 = QTextEdit()
-        self.textInput1.setAcceptRichText(False)
-        self.textInput1.resize(450, 600)
-        self.textInput1.textChanged.connect(self.textInput1Changed)
-        self.timer1 = QTimer()
-        self.timer1.setSingleShot(True)
-        self.timer1.timeout.connect(self.time1Timeout)
+        self.text_input = QTextEdit()
+        self.text_input.setAcceptRichText(False)
+        self.text_input.setFont(fontmono)
+        self.text_input.resize(450, 600)
+        self.text_input.textChanged.connect(self.text_inputChanged)
+        self.timer_wait_to_refresh = QTimer()
+        self.timer_wait_to_refresh.setSingleShot(True)
+        self.timer_wait_to_refresh.timeout.connect(self.refresh_output_when_timeout)
 
-        self.textInput2 = QTextEdit()
-        self.textInput2.resize(450, 600)
-        self.textInput2.setReadOnly(True)
-        #self.textInput2.setStyleSheet("background-color: gray")
+        self.text_output = QTextEdit()
+        self.text_input.setFont(fontmono)
+        self.text_output.resize(450, 600)
+        self.text_output.setReadOnly(True)
+        # self.text_output.setStyleSheet("background-color: gray")
 
         self.hbox = QHBoxLayout()
-        #self.hbox.addStretch()
-        self.hbox.addWidget(self.textInput1)
-        self.hbox.addWidget(self.textInput2)
-        #self.hbox.addStretch()
+        # self.hbox.addStretch()
+        self.hbox.addWidget(self.text_input)
+        self.hbox.addWidget(self.text_output)
+        # self.hbox.addStretch()
 
         self.vbox = QVBoxLayout()
         self.vbox.addLayout(self.subtitle)
@@ -122,10 +138,8 @@ class Ui_MainWindow(object):
 
         self.mainWidget = QWidget()
         self.mainWidget.setLayout(self.vbox)
-        MainWindow.setCentralWidget(self.mainWidget)
+        main_window.setCentralWidget(self.mainWidget)
 
-        MainWindow.setWindowTitle('JC WBXML Parser')
+        main_window.setWindowTitle('JC WBXML Decoder')
 
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-
+        QtCore.QMetaObject.connectSlotsByName(main_window)
